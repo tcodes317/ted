8446e1c1fce2205ec14d51ea2a8654937530b659
tcodes317 <tcodes317@gmail.com>
2024-04-19 07:20:41 -0700

    Done

--- data/import-dev.js
+++ data/import-dev.js
@@ -4,6 +4,7 @@ let fs=require("fs");
 let Movies=require("./../Model/moviesSchema");
 let mongoose=require("mongoose");
 let dotenv=require("dotenv");
+let asyncErrorHandler=require("./../Utils/asyncErrorHandler");
 
 dotenv.config({path: "./../config.env"});
 
@@ -21,4 +22,33 @@ mongoose.connect(process.env.DB_URL, {
 
 let movies=require("./../data/data.json");
 
-let 
+let importMovies=asyncErrorHandler(async(req, res)=>{
+        let imMovies=await Movies.create(movies);
+        
+        res.status(200).json({
+            status: "success",
+            data:{
+                imMovies
+            }
+        })
+    })
+
+let deleteMovies=asyncErrorHandler(async(req, res)=>{
+    let deleteMovies=await Movies.deleteMany({});
+
+    res.status(200).json({
+        status: "success",
+        data:{
+            deleteMovies
+        }
+    })
+})
+
+console.log(process.arvg);
+
+if(process.env.arvg[2] === "--import"){
+    importMovies()
+}
+if(process.env.arvg[2] === "--delete"){
+    deleteMovies();
+}
